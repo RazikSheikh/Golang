c42457e42b9e8992ddc8235cc91bf9f6bc66c001
RazikSheikh <140699337+RazikSheikh@users.noreply.github.com>
2023-07-28 11:25:39 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
-
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PUT, DELETE",
+		AllowCredentials: true,
+	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 
 		return c.JSON(fiber.Map{"Client": c.Context().RemoteAddr()})
